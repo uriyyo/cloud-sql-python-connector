77d922bab94c02d4f46b690d7ba18b2e66111f43
jackwotherspoon <jackwoth@google.com>
2024-06-04 22:21:19 +0000

    fix: error on connect_async() event loop mismatch

--- google/cloud/sql/connector/connector.py
+++ google/cloud/sql/connector/connector.py
@@ -266,6 +266,15 @@ class Connector:
             DnsNameResolutionError: Could not resolve PSC IP address from DNS
                 host name.
         """
+        # check if event loop is running in current thread
+        if self._loop != asyncio.get_running_loop():
+            raise ConnectorLoopError(
+                "Running event loop does not match 'connector._loop'. "
+                "Connector.connect_async() must be called from the event loop "
+                "the Connector was initialized with. If you need to connect "
+                "across event loops/threads please use a new Connector object."
+            )
+
         if self._keys is None:
             self._keys = asyncio.create_task(generate_keys())
         if self._client is None:
